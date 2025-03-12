d546143acfba97d9377d175a65780c0e50b8a637
DaDucIsDaKey <135929516+DaDucIsDaKey@users.noreply.github.com>
2025-03-12 16:52:37 -0700

    Update calculator.js

--- parser/calculator.js
+++ parser/calculator.js
@@ -1,3 +1,18 @@
 function Calculator(untokenized) {
-  
+  let inarr = Tokenize(untokenized);
+  for (let _pass=0;_pass<4) {
+    for(let i=0;i<inarr.length;i++) {
+      if (pass==0 && inarr[i][0] == "(")) {
+        inarr[i]=Calculator(inarr[i].substring(1,inarr[i].length))
+      }
+      if (pass==2 && inarr[i] == "d")) {
+        let r = 0
+        for (let j=0;j<parseInt(inarr[i-1]);j++) {
+          r+=Math.floor(Math.random()*parseInt(inarr[i+1]))+1
+        }
+        inarr.splice(i,3,r.toString())
+        i--;
+      }
+    }
+  }
 }
