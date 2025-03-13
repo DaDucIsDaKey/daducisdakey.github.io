ae84efb1d239da5a64ae9107d4ecfe45c8bab34b
DaDucIsDaKey <135929516+DaDucIsDaKey@users.noreply.github.com>
2025-03-13 10:12:44 -0700

    Update calculator.js

--- parser/calculator.js
+++ parser/calculator.js
@@ -2,10 +2,10 @@ function Calculator(untokenized) {
   let inarr = Tokenize(untokenized);
   for (let _pass=0;_pass<4;_pass++) {
     for(let i=0;i<inarr.length;i++) {
-      if (_pass==0 && inarr[i][0] == "(")) {
+      if (_pass==0 && inarr[i][0] == "(") {
         inarr[i]=Calculator(inarr[i].substring(1,inarr[i].length))
       }
-      if (_pass==1 && inarr[i] == "d")) {
+      if (_pass==1 && inarr[i] == "d") {
         let r = 0
         for (let j=0;j<parseInt(inarr[i-1]);j++) {
           r+=Math.floor(Math.random()*parseInt(inarr[i+1]))+1
