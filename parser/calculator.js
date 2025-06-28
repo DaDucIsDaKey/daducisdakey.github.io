d05d50f8b4f69f46d815315a3d850ca6252cf1c4
Daniil <daniil@lemmings.me>
2025-06-28 12:48:24 -0700

    Class Fix

--- parser/calculator.js
+++ parser/calculator.js
@@ -1,5 +1,5 @@
-function Parse(inString) {
-  const OperatorPrios = Object.freeze({
+class Parser {
+  static OperatorPrios = Object.freeze({
     '+':5,
     '-':5,
     '*':10,
@@ -7,81 +7,97 @@ function Parse(inString) {
     '^':15,
     'd':25
   });
-  const TokenTypes = Object.freeze({
+
+  static TokenTypes = Object.freeze({
     NUMBER:1,
     OPERATOR:2,
     NONE:0,
   });
-  let operands = [];
-  let operators = [];
-  let paras = 0;
-  let currcharnum = 0;
-  let currtoken = "";
-  let currTT=TokenTypes.NONE;
-  for(let i = 0; i<inString.length; i++) {
-    currcharnum = inString.charCodeAt(i);
-    if ((currcharnum>=48 && currcharnum<=57) || currcharnum==46 || (currcharnum==45 && currTT!=TokenTypes.NUMBER)) {
-      currtoken+=inString[i];
-      currTT=TokenTypes.NUMBER;
-    }
-    else if (currcharnum==42 || currcharnum==43 || currcharnum==45 || currcharnum==47 || currcharnum==94 || currcharnum==100) {
-      if (currTT==TokenTypes.NUMBER) {
-      	operands.push(+currtoken);
-      	currtoken="";
-      }
-      let priority = OperatorPrios[inString[i]]+1000*paras;
-      while (operators.length>0 && priority<=operators.at(-1)[1]) {
-      	Evaluate(operators.pop(),operands);
-      }
-      operators.push([currcharnum,priority]);
-      currTT=TokenTypes.NONE;
-    }
-    else if (currcharnum==40) {
-    	paras+=1;
-    	//Add multiplication before parenthesis?
-    }
-    else if (currcharnum==41) {
-    	paras-=1;
-    }    
+
+  constructor() {
+    this.Reset();
+    this.inString="";
   }
-  operands.push(+currtoken);
-  while (operators.length>0) {
-    Evaluate(operators.pop(),operands);
+
+  Reset() {
+    this.operands=[];
+    this.operators=[];
+    this.paras=0;
+    this.currtoken = "";
+    this.currTT=0;  
   }
-  return operands.pop();
+
+  Parse(inString) {
+    this.Reset();
+    for(let i = 0; i<inString.length; i++) {
+      let currcharnum = inString.charCodeAt(i);
+      if ((currcharnum>=48 && currcharnum<=57) || currcharnum==46 || (currcharnum==45 && this.currTT!=Parser.TokenTypes.NUMBER)) {
+        this.currtoken+=inString[i];
+        this.currTT=Parser.TokenTypes.NUMBER;
+      }
+      else if (currcharnum==42 || currcharnum==43 || currcharnum==45 || currcharnum==47 || currcharnum==94 || currcharnum==100) {
+        if (this.currTT==Parser.TokenTypes.NUMBER) {
+          this.operands.push(+this.currtoken);
+          this.currtoken="";
+        }
+        let priority = Parser.OperatorPrios[inString[i]]+1000*this.paras;
+        while (this.operators.length>0 && priority<=this.operators.at(-1)[1]) {
+          this.Evaluate();
+        }
+        this.operators.push([currcharnum,priority]);
+        this.currTT=Parser.TokenTypes.NONE;
+      }
+      else if (currcharnum==40) {
+        this.paras+=1;
+        //Add multiplication before parenthesis?
+      }
+      else if (currcharnum==41) {
+        this.paras-=1;
+      }    
+    }
+    this.operands.push(+this.currtoken);
+    while (this.operators.length>0) {
+      this.Evaluate();
+    }
+    return this.operands.pop();
 }
 
-function Evaluate(op,operands) {
-  if (op[0]==42) {
-    operands.push(operands.pop()*operands.pop());
-  }
-  else if (op[0]==43) {
-    operands.push(operands.pop()+operands.pop());
-  }
-  else if (op[0]==45) {
-    operands.push(-operands.pop()+operands.pop());
-  }
-  else if (op[0]==47) {
-    let b = operands.pop();
-    let a = operands.pop();
-    operands.push(a/b);
-  }
-  else if (op[0]==94) {
-    let b = operands.pop();
-    let a = operands.pop();
-    operands.push(a**b);
-  }
-  else if (op[0]==100) {
-    let b = operands.pop();
-    let a = operands.pop();
-    let o=0;
-    for (let i = 0; i<a; i++) {
-      o+=Math.floor(Math.random()*b)+1;
+  Evaluate() {
+    let op=this.operators.pop();
+    if (op[0]==42) {
+      this.operands.push(this.operands.pop()*this.operands.pop());
     }
-    operands.push(o);
+    else if (op[0]==43) {
+      this.operands.push(this.operands.pop()+this.operands.pop());
+    }
+    else if (op[0]==45) {
+      this.operands.push(-this.operands.pop()+this.operands.pop());
+    }
+    else if (op[0]==47) {
+      let b = this.operands.pop();
+      let a = this.operands.pop();
+      this.operands.push(a/b);
+    }
+    else if (op[0]==94) {
+      let b = this.operands.pop();
+      let a = this.operands.pop();
+      this.operands.push(a**b);
+    }
+    else if (op[0]==100) {
+      let b = this.operands.pop();
+      let a = this.operands.pop();
+      let o=0;
+      for (let i = 0; i<a; i++) {
+        o+=Math.floor(Math.random()*b)+1;
+      }
+      this.operands.push(o);
+    }
   }
 }
-console.log(Parse("100000000d6"));
+
+var psr = new Parser();
+
+console.log(psr.Parse("5+2-3*5"));
 
 
 
